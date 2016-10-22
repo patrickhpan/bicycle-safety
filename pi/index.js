a86a66336410960a2b1df66c823928bb882e93cb
Patrick Pan <patrickpan@college.harvard.edu>
2016-10-22 15:41:34 -0400

    added callback for LED and logging data

--- pi/index.js
+++ pi/index.js
@@ -1,8 +1,23 @@
+const fs = require('fs');
 const GPIO = require('pigpio').Gpio;
-const CONFIG = require('./config');
 const LED = require('./hardware/LED');
+const GPSReader = require('./hardware/GPSReader');
+
+const CONFIG = require('./config');
 
 let led = new LED(CONFIG.GPIO.LED);
-setInterval(function() { 
-	led.toggle()
-}, 500)
+let stream = fs.createWriteStream(`gps-${+new Date()}.txt`);
+
+let callback = data => {
+	if (data[0] !== 0 && data[1] !== 0) {
+		led.on()
+		stream.write(`${new Date().toString()}: data[0], data[1]\n`)
+	} else {
+		led.off()
+		stream.write(`${new Date().toString()}: failed to lock`)
+	}
+}
+
+stream.once('open', () => {
+	let gps = new GPSReader(CONFIG.GPS.file, callback);
+})
