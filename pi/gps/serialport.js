2de71147ef15429a762280e163df12cd898de85f
Patrick Pan <patrickpan@college.harvard.edu>
2016-10-22 01:20:19 -0400

    Added parser for gps serial data

--- pi/gps/serialport.js
+++ pi/gps/serialport.js
@@ -5,6 +5,24 @@ var gpsPort = new SerialPort(CONFIG.GPS.file, {
     parser: SerialPort.parsers.readline('\n')
 });
 
+let filterGPGLL = line => !!(line.match(/^\$GPGLL/));
+let gpgllToLocation = line => {
+    let values = line.split(',');
+    let latitude = Number(values[1]);
+    let longitude = Number(values[3]);
+    if (values[2] === 'S') {
+        latitude *= -1;
+    } 
+    if (values[4] === 'W') {
+        longitude *= -1;
+    } 
+    return [latitude, longitude];
+}
+
 gpsPort.on('data', data => {
-    console.log(`Received ${data}`);
+    if (filterGPGLL(data)) {
+        let coordinate = gpgllToLocation(data);
+        console.log(`(${coordinate[0]}, ${coordinate[1]})`);
+    }
+    
 })
